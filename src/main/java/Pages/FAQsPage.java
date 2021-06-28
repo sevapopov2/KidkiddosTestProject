1895b90dc0aa609973155bd576dd6eaed20ee17d
Vsevolod Popov <sevapopov13@gmail.com>
2021-06-28 09:39:09 +0300

    FAQ's page test was fixed.

--- src/main/java/Pages/FAQsPage.java
+++ src/main/java/Pages/FAQsPage.java
@@ -1,7 +1,7 @@
 package Pages;
 
 public class FAQsPage extends BasePage{
-    private static final String info_heading = "//h1[contains(text(), 'FAQ/']";
+    private static final String info_heading = "//h1[contains(text(), 'FAQ/')]";
 
     public boolean isHeadingVisible() {
         return elementExists(info_heading);
